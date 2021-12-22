8e73070feb776f1e53e69f84341feebfe5083de7
prashant naik <real.hyper.clocked@gmail.com>
2021-12-22 23:02:02 +0530

    Adding home page

---
+++ src/components/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainNavigation from '../../UI/MainNavigation';
+
+const Home = (props) => {
+return (
+    <React.Fragment>
+        <MainNavigation/>
+    </React.Fragment>
+)
+
+}
+
+export default Home;
