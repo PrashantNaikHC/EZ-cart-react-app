13106b155502e766478ca69f4b4a42ea8f802862
prashant naik <real.hyper.clocked@gmail.com>
2021-12-23 16:17:09 +0530

    Adding add to cart button

--- src/components/UI/ProductItem/ProductItem.js
+++ src/components/UI/ProductItem/ProductItem.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  console.log("results getting", props.products);
   return (
     <ul>
       {props.products.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li className={classes.item} key={item.id}>
+          <img src={item.image} style={{ width: "25%" }}></img>
+          <div className={classes.detail}>
+            <h4>{item.title}</h4>
+            <p>{item.description}</p>
+            <input type="number" min="0" max="10"></input>
+            <Button>Add to cart</Button>
+          </div>
+          <br />${item.price}
+        </li>
       ))}
     </ul>
   );
